b6ae6da28e5d50aeb23cbf2f69a3ff6a2e9b8c34
Juan Silva <juanse909@hotmail.com>
2024-08-18 19:43:31 -0500

    orderByName function created

--- utils/orderByName.js
+++ utils/orderByName.js
@@ -1,1 +1,13 @@
-import students from "../data/students";
+import students from "../data/students.js";
+
+export const orderByName = (students) => {
+    let order = students.sort((a, b) => {
+        if (a.name < b.name) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return order;
+};
+console.log(orderByName(students));
