c9d44df399f6b448c00011ed67b7bffbd46da591
Juan Silva <juanse909@hotmail.com>
2024-08-18 19:44:46 -0500

    index update to render students data

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import students from "./data/students.js";
+import {renderStudents} from "./utils/renderStudents.js";
 import {filterHighGrade} from "./utils/filterHighGrade.js";
+import {calculateMeanGrade} from "./utils/calculateMeanGrade.js";
+import {orderByName} from "./utils/orderByName.js";
+import {organizeByCourse} from "./utils/organizeByCourse.js";
 
-console.log(filterHighGrade(students));
+const container = document.querySelector('.student-container');
+
+students.forEach(student => {
+    const { id, name, age, grade, course, calculateMeanGrade } = student;
+    const studentElement = renderStudents(id, name, age, grade, course, calculateMeanGrade);
+    container.appendChild(studentElement);
+});
